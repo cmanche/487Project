c293f545aa9af11636e6c621981809025d25f681
William Chimbay <123396327+will-chimbay@users.noreply.github.com>
2024-12-16 19:49:28 -0500

    Update bat_n_ball.vhd

--- bat_n_ball.vhd
+++ bat_n_ball.vhd
@@ -20,7 +20,12 @@ ENTITY bat_n_ball IS
         -- CHANGE: Added score outputs for both players
         score1 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
         score2 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-        speed_mode : IN STD_LOGIC_VECTOR(2 DOWNTO 0)  -- speed mode input
+        speed_mode : IN STD_LOGIC_VECTOR(2 DOWNTO 0);  -- speed mode input
+        -- CHANGE: Added color inversion controls
+        invert_background : IN STD_LOGIC;
+        invert_paddles : IN STD_LOGIC;
+        -- CHANGE: Added random bounce control
+        random_bounce : IN STD_LOGIC
     );
 END bat_n_ball;
 
@@ -62,23 +67,67 @@ ARCHITECTURE Behavioral OF bat_n_ball IS
     SIGNAL score1_reg : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";  -- Initialize to zero
     SIGNAL score2_reg : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";  -- Initialize to zero
     
+    SIGNAL score1_count : INTEGER RANGE 0 TO 15 := 0;  -- Extended range to handle 10-15
+    SIGNAL score2_count : INTEGER RANGE 0 TO 15 := 0;
+
+    
+    -- CHANGE: serve control signals
+    SIGNAL serve_count : STD_LOGIC := '0';  -- Toggles every serve
+    SIGNAL serve_dir : STD_LOGIC := '0';    -- Toggles every two serves
+    SIGNAL serve_angle : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";  -- For varying 
+    
     SIGNAL center_line : STD_LOGIC;
+    
+    -- CHANGE: Added random number generator signals for paddle bounces
+    SIGNAL rand_counter : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
+    SIGNAL rand_value : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    
+    -- CHANGE: Added color control signals
+    SIGNAL background_color : STD_LOGIC;
+    SIGNAL ball_color : STD_LOGIC;
+    SIGNAL paddle_color : STD_LOGIC;
+    
 BEGIN
 --    red <= NOT bat_on; -- color setup for red ball and cyan bat on white background
 --    green <= NOT ball_on;
 --    blue <= NOT ball_on;
+    -- CHANGE: Color control based on inversion settings
+    center_line <= '1' when (pixel_col >= 398 and pixel_col <= 402) else '0';
+    background_color <= invert_background;
+    ball_color <= NOT invert_background;
+    paddle_color <= '1' WHEN invert_paddles = '0' ELSE '0';
 -- COlor reassignment + center line
 -- no color for ball = black
-    red <= bat1_on  OR center_line;
-    green <= center_line OR (NOT (bat1_on OR bat2_on OR ball_on)); -- Green background
-    blue <= bat2_on OR center_line;
-    
-    center_line <= '1' when (pixel_col >= 398 and pixel_col <= 402) else '0';
+--    red <= bat1_on  OR center_line;
+--    green <= center_line OR (NOT (bat1_on OR bat2_on OR ball_on)); -- Green background
+--    blue <= bat2_on OR center_line;
+    -- CHANGE: Modified color assignments
+    red <= (bat1_on AND NOT invert_paddles) OR  -- Left paddle red when not inverted
+           (bat2_on AND invert_paddles) OR      -- Right paddle red when inverted
+           center_line;                         -- Center line always white
+           
+    green <= (NOT (bat1_on OR bat2_on OR ball_on) AND NOT invert_background) OR  -- Green background when not inverted
+             (ball_on AND invert_background) OR                                   -- Green ball when inverted
+             center_line;                                                        -- Center line always white
+             
+    blue <= (bat2_on AND NOT invert_paddles) OR  -- Right paddle blue when not inverted
+            (bat1_on AND invert_paddles) OR      -- Left paddle blue when inverted
+            center_line;                         -- Center line always white
     
     --  score update
     score1 <= score1_reg;
     score2 <= score2_reg;    
         -- Process to set speed based on switches
+    -- CHANGE: Random number generator process
+    random_gen : PROCESS(v_sync)
+    BEGIN
+        IF rising_edge(v_sync) THEN
+            rand_counter <= rand_counter + 1;
+        END IF;
+    END PROCESS;
+    
+    rand_value <= rand_counter;
+        
     process(speed_mode)
     begin
         case speed_mode is
@@ -169,15 +218,52 @@ BEGIN
 --        END IF;
 
     -- base off, but now logic is rotated ess3entially
+--        IF serve = '1' AND game_on = '0' THEN
+--            game_on <= '1';
+--            ball_y_motion <= ball_speed;
+--            -- CHANGE: Randomize initial direction
+--            IF ball_x < 400 THEN
+--                ball_x_motion <= ball_speed;
+--            ELSE
+--                ball_x_motion <= (NOT ball_speed) + 1;
+--            END IF;
+--        END IF;
+
+        -- CHANGE: enhanced serve behavior with direction alternation and varying angles
         IF serve = '1' AND game_on = '0' THEN
             game_on <= '1';
-            ball_y_motion <= ball_speed;
-            -- CHANGE: Randomize initial direction
-            IF ball_x < 400 THEN
-                ball_x_motion <= ball_speed;
-            ELSE
-                ball_x_motion <= (NOT ball_speed) + 1;
+            serve_count <= NOT serve_count;
+            
+            -- Update serve direction every two serves
+            IF serve_count = '1' THEN
+                serve_dir <= NOT serve_dir;
             END IF;
+            
+            -- Update angle selection
+            serve_angle <= serve_angle + "01";
+            
+            -- Set initial ball direction
+            IF serve_dir = '0' THEN  -- Serve left
+                ball_x_motion <= (NOT ball_speed) + 1;  -- Left
+                CASE serve_angle IS
+                    WHEN "00" => ball_y_motion <= (NOT (ball_speed - 2)) + 1;  -- Slight up
+                    WHEN "01" => ball_y_motion <= ball_speed;                   -- Down
+                    WHEN "10" => ball_y_motion <= (NOT ball_speed) + 1;        -- Up
+                    WHEN OTHERS => ball_y_motion <= (ball_speed - 2);          -- Slight down
+                END CASE;
+            ELSE  -- Serve right
+                ball_x_motion <= ball_speed;  -- Right
+                CASE serve_angle IS
+                    WHEN "00" => ball_y_motion <= (NOT (ball_speed - 2)) + 1;  -- Slight up
+                    WHEN "01" => ball_y_motion <= ball_speed;                   -- Down
+                    WHEN "10" => ball_y_motion <= (NOT ball_speed) + 1;        -- Up
+                    WHEN OTHERS => ball_y_motion <= (ball_speed - 2);          -- Slight down
+                END CASE;
+            END IF;
+            
+            -- Reset ball position
+            ball_x <= CONV_STD_LOGIC_VECTOR(400, 11);
+            ball_y <= CONV_STD_LOGIC_VECTOR(300, 11);
         END IF;
         
         -- Bounce off top and bottom walls
@@ -196,35 +282,77 @@ BEGIN
 --                ball_y_motion <= (NOT ball_speed) + 1; -- set vspeed to (- ball_speed) pixels
 --        END IF;
 
+        -- SCORING LOGIC
         IF ball_x + bsize >= 800 THEN  -- Right wall, point for player 1
-            score1_reg <= score1_reg + "0001";  -- Explicit 4-bit addition
+            score1_count <= score1_count + 1;
+            -- Convert integer directly to the correct hex value for display
+            score1_reg <= CONV_STD_LOGIC_VECTOR(score1_count + 1, 4);  -- Add 1 here for immediate update
             game_on <= '0';
             ball_x <= CONV_STD_LOGIC_VECTOR(400, 11);
             ball_y <= CONV_STD_LOGIC_VECTOR(300, 11);
         ELSIF ball_x <= bsize THEN     -- Left wall, point for player 2
-            score2_reg <= score2_reg + "0001";  -- Explicit 4-bit addition
+            score2_count <= score2_count + 1;
+            -- Convert integer directly to the correct hex value for display
+            score2_reg <= CONV_STD_LOGIC_VECTOR(score2_count + 1, 4);  -- Add 1 here for immediate update
             game_on <= '0';
             ball_x <= CONV_STD_LOGIC_VECTOR(400, 11);
             ball_y <= CONV_STD_LOGIC_VECTOR(300, 11);
         END IF;
+
         
 --         with l & r handling
 --         CHANGE: Handle bat collisions for both bats
-      --   Left bat collision
-        IF (ball_x - bsize <= bat1_x + bat_w) AND
-           (ball_x + bsize >= bat1_x - bat_w) AND
-           (ball_y + bsize >= bat1_y - bat_h) AND
-           (ball_y - bsize <= bat1_y + bat_h) THEN
-            ball_x_motion <= ball_speed;
-        END IF;
+--      --   Left bat collision
+--        IF (ball_x - bsize <= bat1_x + bat_w) AND
+--           (ball_x + bsize >= bat1_x - bat_w) AND
+--           (ball_y + bsize >= bat1_y - bat_h) AND
+--           (ball_y - bsize <= bat1_y + bat_h) THEN
+--            ball_x_motion <= ball_speed;
+--        END IF;
         
-      --   Right bat collision
-        IF (ball_x + bsize >= bat2_x - bat_w) AND
-           (ball_x - bsize <= bat2_x + bat_w) AND
-           (ball_y + bsize >= bat2_y - bat_h) AND
-           (ball_y - bsize <= bat2_y + bat_h) THEN
-            ball_x_motion <= (NOT ball_speed) + 1;
+--      --   Right bat collision
+--        IF (ball_x + bsize >= bat2_x - bat_w) AND
+--           (ball_x - bsize <= bat2_x + bat_w) AND
+--           (ball_y + bsize >= bat2_y - bat_h) AND
+--           (ball_y - bsize <= bat2_y + bat_h) THEN
+--            ball_x_motion <= (NOT ball_speed) + 1;
+--        END IF;
+    -- UPDATED
+    -- Left bat collision with random bounce
+    IF (ball_x - bsize <= bat1_x + bat_w) AND
+       (ball_x + bsize >= bat1_x - bat_w) AND
+       (ball_y + bsize >= bat1_y - bat_h) AND
+       (ball_y - bsize <= bat1_y + bat_h) THEN
+        ball_x_motion <= ball_speed;
+        IF random_bounce = '1' THEN
+            CASE rand_value IS
+                WHEN "000" => ball_y_motion <= (NOT (ball_speed - 2)) + 1;
+                WHEN "001" => ball_y_motion <= ball_speed;
+                WHEN "010" => ball_y_motion <= (NOT ball_speed) + 1;
+                WHEN "011" => ball_y_motion <= (ball_speed - 2);
+                WHEN "100" => ball_y_motion <= (NOT (ball_speed + 1)) + 1;
+                WHEN OTHERS => ball_y_motion <= ball_speed;
+            END CASE;
         END IF;
+    END IF;
+    
+    -- Right bat collision with random bounce
+    IF (ball_x + bsize >= bat2_x - bat_w) AND
+       (ball_x - bsize <= bat2_x + bat_w) AND
+       (ball_y + bsize >= bat2_y - bat_h) AND
+       (ball_y - bsize <= bat2_y + bat_h) THEN
+        ball_x_motion <= (NOT ball_speed) + 1;
+        IF random_bounce = '1' THEN
+            CASE rand_value IS
+                WHEN "000" => ball_y_motion <= (NOT (ball_speed - 2)) + 1;
+                WHEN "001" => ball_y_motion <= ball_speed;
+                WHEN "010" => ball_y_motion <= (NOT ball_speed) + 1;
+                WHEN "011" => ball_y_motion <= (ball_speed - 2);
+                WHEN "100" => ball_y_motion <= (NOT (ball_speed + 1)) + 1;
+                WHEN OTHERS => ball_y_motion <= ball_speed;
+            END CASE;
+        END IF;
+    END IF;
             
 --        -- compute next ball vertical position
 --        -- variable temp adds one more bit to calculation to fix unsigned underflow problems
